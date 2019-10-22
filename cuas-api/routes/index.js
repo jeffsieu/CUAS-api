9889b3cecd5f74db5f46c9df5eb8a501a48de86b
Jonathan <jonathanng1989@gmail.com>
2019-10-22 21:54:14 +0800

    Version 0.0.7
    - Updated more API examples

--- cuas-api/routes/index.js
+++ cuas-api/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var TYPES = require('tedious').TYPES;
 
 /* GET task listing. */
-router.get('/', function (req, res) {
+router.get('/customers', function (req, res) {
 
     req.sql("select * from InfoReporting for json path")
         .into(res, '[]');
@@ -49,4 +49,46 @@ router.delete('/:id', function (req, res) {
         .param('id', req.params.id, TYPES.Int)
         .exec(res);
 
+});*/
+
+/*//rest api to get all customers
+app.get('/customer', function (req, res) {
+    connection.query('select * from customer', function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results));
+    });
+});
+//rest api to get a single customer data
+app.get('/customer/:id', function (req, res) {
+    connection.query('select * from customers where Id=?', [req.params.id], function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results));
+    });
+});
+
+//rest api to create a new customer record into mysql database
+app.post('/customer', function (req, res) {
+    var params = req.body;
+    console.log(params);
+    connection.query('INSERT INTO customer SET ?', params, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results));
+    });
+});
+
+//rest api to update record into mysql database
+app.put('/customer', function (req, res) {
+    connection.query('UPDATE `customer` SET `Name`=?,`Address`=?,`Country`=?,`Phone`=? where `Id`=?', [req.body.Name, req.body.Address, req.body.Country, req.body.Phone, req.body.Id], function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results));
+    });
+});
+
+//rest api to delete record from mysql database
+app.delete('/customer', function (req, res) {
+    console.log(req.body);
+    connection.query('DELETE FROM `customer` WHERE `Id`=?', [req.body.Id], function (error, results, fields) {
+        if (error) throw error;
+        res.end('Record has been deleted!');
+    });
 });*/
